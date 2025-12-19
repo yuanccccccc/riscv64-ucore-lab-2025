221e5487beb51d1057f1359d591908a046b26253
xiao <2212449@mail.nankai.edu.cn>
2025-12-19 22:00:42 +0800

    更新lab7 8

--- lab7_new/tools/grade.sh
+++ lab7_new/tools/grade.sh
@@ -341,6 +341,20 @@ default_check() {
     'check_boot_pgdir() succeeded!'				\
     'kmalloc_init() succeeded!'                             \
     'check_vma_struct() succeeded!'                             \
+    'page fault at 0x00000100: K/W'                             \
+    'check_pgfault() succeeded!'                                \
+    'check_vmm() succeeded.'					\
+    'page fault at 0x00001000: K/W'            \
+    'page fault at 0x00002000: K/W'            \
+    'page fault at 0x00003000: K/W'            \
+    'page fault at 0x00004000: K/W'            \
+    'write Virt Page e in fifo_check_swap'			\
+    'page fault at 0x00005000: K/W'		\
+    'page fault at 0x00001000: K/W'		\
+    'page fault at 0x00002000: K/W'		\
+    'page fault at 0x00003000: K/W'		\
+    'page fault at 0x00004000: K/W'		\
+    'check_swap() succeeded!'					\
     '++ setup timer interrupts'
 }
 
